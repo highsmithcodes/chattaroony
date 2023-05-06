7d9f4814dd0a7b646ca8b52a1a744b89456a2c58
highsmithcodes <ezrahighsmith@gmail.com>
2023-05-06 13:50:39 -0600

    add next steps

--- client/src/components/Directory/index.js
+++ client/src/components/Directory/index.js
@@ -1,19 +1,32 @@
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery } from '@apollo/client';
+// import { ApolloClient, InMemoryCache, ApolloProvider, useQuery } from '@apollo/client';
 import DisplayData from '../DisplayData';
-const dotenv = require('dotenv/config');
+// const dotenv = require('dotenv/config');
+import * as Realm from "realm-web";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 
 function Directory() {
 
+    const graphqlUri = `mongodb+srv://ezrahighsmith:t7UjAQ7NN37bV9Hw@cluster0.cyx9iuz.mongodb.net/?retryWrites=true&w=majority`;
+
     const client = new ApolloClient({
+        link: new HttpLink({
+            uri: graphqlUri,
+        }),
         cache: new InMemoryCache(),
-        uri: process.env.DATABASE_URL
     })
+    
 
     return ( 
         <ApolloProvider client={client}>
             <div>
                 <div>
                     <h1>List Of Movies</h1>
+                    {/* Display Data */}
                     <DisplayData />
                 </div>
             </div>
