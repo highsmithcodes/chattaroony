b6f1c132664596526004ee1332e11c755ecec9df
highsmithcodes <ezrahighsmith@gmail.com>
2023-05-15 13:54:41 -0600

    add new build && clean up

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,6 @@ function App() {
   return (
       <div className="App">
         <AuthContextProvider>
-          {/* Add Jest */}
           <Header/>
           <Routes>
             <Route exact path="/" element={<Protected><Homepage/></Protected>} />
