e0806d9bd4bf07d682a2b7051bb79b3e191cb4c0
Highsmith Codes <ezrahighsmith@gmail.com>
2023-06-01 21:29:12 +0000

    update header colors

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -29,7 +29,7 @@ function Header() {
     }
 
     return ( 
-      <Disclosure as="nav" className="bg-gray-800">
+      <Disclosure as="nav" className="bg-slate-50">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -67,7 +67,7 @@ function Header() {
                         key={item.name}
                         to={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.current ? 'bg-gray-900 text-white' : 'text-slate-300 hover:bg-slate-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
